3de80ae9371e0a3f712e40f8933ac4ece27bb164
Guillaume Lodi <guillaume.lodi@gmail.com>
2019-04-20 17:40:27 +0200

    Style product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Route, NavLink } from "react-router-dom";
+
 import ProductsList from './Products/ProductList';
+import ProductDetails from './Products/ProductDetails';
+
 import './App.css';
 
 class App extends Component {
@@ -8,13 +11,13 @@ class App extends Component {
     return (
       <div className="App">
         <nav>
-          <NavLink exact to="/" activeCLassName="active">Home</NavLink>
-          <NavLink to="/list" activeCLassName="active">Products</NavLink>
-          <NavLink to="/about" activeCLassName="active">About</NavLink>
+          <NavLink exact to="/" activeClassName="active">Home</NavLink>
+          <NavLink to="/list" activeClassName="active">Products</NavLink>
+          <NavLink to="/about" activeClassName="active">About</NavLink>
         </nav>
         <Route exact path="/" component={() => <h1>Home</h1>} />
         <Route exact path="/list" component={ProductsList} />
-        <Route exact path="/list/:id" component={() => <h1>Simple product</h1>} />
+        <Route exact path="/list/:id" component={ProductDetails} />
         <Route exact path="/about" component={() => <h1>About (todo)</h1>} />
       </div>
     );
--- src/Products/ProductDetails.jsx
+++ src/Products/ProductDetails.jsx
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
+import './ProductDetails.css';
 
 class ProductDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: undefined,
+      image: undefined,
+      name: undefined,
+      price: undefined,
+      product_id: undefined
+    };
+  }
 
+  componentWillMount() {
+    const url = "https://s3-eu-west-1.amazonaws.com/developer-application-test/cart/{product_id}/detail";
+    const product_id = parseInt(this.props.match.params.id);
+
+    fetch(url.replace("{product_id}", product_id))
+      .then(res => {
+        res.json().then(data => {
+          this.setState({ ...data });
+          console.log(data);
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const { name, description, image, price } = this.state;
+    return (
+      <div className="container product">
+        <img className="product-image" src={image} alt={name} />
+        <div className="product-content">
+          <h1>{name}</h1>
+          <p className="product-content-price">{price}$</p>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default ProductDetails;
