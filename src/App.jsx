4946bfcd87c07a358ecd1375c2f4e1e155849261
Guillaume Lodi <guillaume.lodi@gmail.com>
2019-04-20 17:06:17 +0200

    simple style nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import { Route, Link } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import ProductsList from './Products/ProductList';
 import './App.css';
-import Loader from './Utils/Loader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <nav>
-          <Link to="/">Home</Link> |
-          <Link to="/list"> Products</Link> |
-          <Link to="/about"> About</Link>
+          <NavLink exact to="/" activeCLassName="active">Home</NavLink>
+          <NavLink to="/list" activeCLassName="active">Products</NavLink>
+          <NavLink to="/about" activeCLassName="active">About</NavLink>
         </nav>
-        <Route exact path="/" component={() => <h1>Home</h1>} />
+        <Route path="/" component={() => <h1>Home</h1>} />
         <Route exact path="/list" component={ProductsList} />
         <Route exact path="/list/:id" component={() => <h1>Simple product</h1>} />
-        <Route path="/about" component={() => <h1>About (todo)</h1>} />
+        <Route exact path="/about" component={() => <h1>About (todo)</h1>} />
       </div>
     );
   }
