4234d7b80aea5f3993a0e9bdd19bc117a0da375c
Guillaume Lodi <guillaume.lodi@gmail.com>
2019-04-21 14:53:54 +0200

    Some changes on style

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,12 @@ class App extends Component {
           <NavLink to="/list" activeClassName="active">Products</NavLink>
           <NavLink to="/about" activeClassName="active">About</NavLink>
         </nav>
-        <Route exact path="/" component={() => <h1>Home</h1>} />
+        <Route exact path="/" component={() => <h1 className="page-title">Home</h1>} />
         <Route exact path="/list" component={ProductsList} />
         <Route exact path="/list/:id" component={ProductDetails} />
-        <Route exact path="/about" component={() => <h1>About (todo)</h1>} />
+        <Route exact path="/about" component={() => <h1 className="page-title">About</h1>} />
       </div>
     );
   }
 }
-
 export default App;
